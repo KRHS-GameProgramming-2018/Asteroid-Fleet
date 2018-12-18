42ca22933859a3d5ee5656d29e212906d3431e7e
Brandon Bullis <brandon.bullis@kearsarge.org>
2018-12-18 08:05:18 -0500

    Fixing Asteroid.py Document

--- Asteroid.py
+++ Asteroid.py
@@ -1,47 +1,74 @@
-# Test Jake Bender
-#Different sizes/velocities/startPos(random)
-
-import pygame, sys, math, random
-from Ship import *
-from PlayerShip import *
-from getInputAsteroid import *
-pygame.init()
-
-clock = pygame.time.Clock()
-
-pygame.mouse.set_visible(True)
-
-width = 800
-height = 1100
-size = width, height
-
-screen = pygame.display.set_mode(size)
+import pygame, sys, math
 
 
-player1 = PlayerShip(5, [width/2, height/2])
+class Asteroid():
+    def __init__(self, image, speed=[0,5], startPos=[random.randint(1,799),-10]):
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.speedx = speed[0]
+        self.speedy = speed[1]
+        self.speed = [self.speedx, self.speedy]
+        self.rect = self.rect.move(startPos)
+        self.radius = (self.rect.width/2 + self.rect.height/2)/2
+        self.didBounceX = False
+        self.didBounceY = False
+    
+    def getDist(self, pt):
+        x1 = self.rect.centerx
+        y1 = self.rect.centery
+        x2 = pt[0]
+        y2 = pt[1]
+        return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+            
+    def update(self, size):
+        self.didBounceX = False
+        self.didBounceY = False
+        self.move()
+        self.bounceWall(size)
+    
+    def move(self):
+        self.speed = [self.speedx, self.speedy]
+        self.rect = self.rect.move(self.speed)
+        
+    def bounceWall(self, size):
+        width = size[0]
+        height = size[1]
+        if self.rect.left < 0 or self.rect.right > width:
+            if not self.didBounceX:
+                self.speedx = -self.speedx
+                self.didBounceX = True
+        if self.rect.top < 0 or self.rect.bottom > height:
+            if not self.didBounceY:
+                self.speedy = -  self.speedy
+                self.didBounceY = True
+            
+    def collide(self, other):
+        if not(self == other):
+            if self.rect.right > other.rect.left:
+                if self.rect.left < other.rect.right:
+                    if self.rect.top < other.rect.bottom:
+                        if self.rect.bottom > other.rect.top:
+                            if self.radius + other.radius > self.getDist(other.rect.center):
+                                if not self.didBounceX:
+                                    if self.speedx > 1: #right
+                                        if self.rect.centerx < other.rect.centerx:
+                                            self.speedx = -self.speedx
+                                            self.didBounceX = True
+                                    if self.speedx < 1: #left
+                                        if self.rect.centerx > other.rect.centerx:
+                                            self.speedx = -self.speedx
+                                            self.didBounceX = True
+                                            
+                                if not self.didBounceY:
+                                    if self.speedy > 1: #down
+                                        if self.rect.centery < other.rect.centery:
+                                            self.speedy = -self.speedy
+                                            self.didBounceY = True
+                                    if self.speedy < 1: #up
+                                        if self.rect.centery > other.rect.centery:
+                                            self.speedy  = self.speedy
+                                            self.didBounceY = True     
 
-while True:
-    for event in pygame.event.get():
-        #print event.type
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                player1.go("north")
-            if event.key == pygame.K_a:
-                player1.go("west")
-            if event.key == pygame.K_s:
-                player1.go("south")
-            if event.key == pygame.K_d:
-                player1.go("east")
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                player1.go("northU")
-            if event.key == pygame.K_a:
-                player1.go("westU")
-            if event.key == pygame.K_s:
-                player1.go("southU")
-            if event.key == pygame.K_d:
-                player1.go("eastU")
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                player1.
+                                return True
+        return False
+
