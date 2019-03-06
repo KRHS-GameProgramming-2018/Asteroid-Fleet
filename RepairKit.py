a086b30b3f785f4e4856cff804da3db97491ee03
Jared Fortier <Jared.Fortier@kearsarge.org>
2019-03-06 08:00:25 -0500

    fixed death screen and added in  repair kit

--- RepairKit.py
+++ RepairKit.py
@@ -1,13 +1,19 @@
-import pygame, sys, math
+import pygame, sys, math, random
+from Ship import *
 width = 1100
 height = 900
 
 class RepairKit():
-    def __init__(self, image, pos):
-        self.image = pygame.transform.scale(pygame.image.load(image),[200,50])
-        self.rect = self.image.get_rect()
-
-    def setPos(self, pos):
-        pos = [600,200]
-
-#HUD = HeadsUpDisplay("Screen Display/HUD/HealthBar/images/health.100%.png", pos = [600,200])
+    def __init__(self, image, startPos = [0,0]):
+        self.image = pygame.transform.scale(pygame.image.load(image),[50,50])
+        self.rect = self.image.get_rect(center=[random.randint(50,width-50),(300)])
+        self.living = True
+  
+    def collideShip(self, other):
+        if not(self == other):
+            if self.rect.right > other.rect.left:
+                if self.rect.left < other.rect.right:
+                    if self.rect.top < other.rect.bottom:
+                        if self.rect.bottom > other.rect.top:
+                            if self.radius + other.radius > self.getDist(other.rect.center):
+                                self.living = False
