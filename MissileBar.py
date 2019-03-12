35a57d73fb831a4b5bbef4e18251ebe796833d1c
Jared Fortier <Jared.Fortier@kearsarge.org>
2019-03-12 07:46:50 -0400

    missile lives

--- MissileBar.py
+++ MissileBar.py
@@ -5,18 +5,16 @@ width = 1100
 height = 900
 
 class MissileBar():
-    def __init__(self, health, startPos = [0,0]):
-        self.images = [pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.png"),[200,50]),
-                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.png"),[200,50]),
-                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.png"),[200,50]),
-                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.png"),[200,50]),
-                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.png"),[200,50])]         
+    def __init__(self, stock, startPos = [0,0]):
+        self.images = [pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.1.png"),[200,50]),
+                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.2.png"),[200,50]),
+                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.3.png"),[200,50])]         
                         
         
-        self.image = self.images[health]
+        self.image = self.images[stock]
         self.rect = self.image.get_rect(center=startPos)
 
     def update(self, health):
-        self.image = self.images[health]
+        self.image = self.images[stock]
         
          
