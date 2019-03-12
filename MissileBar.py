7276deb9e662241e130c14de8d8f71e55c328557
Jared Fortier <Jared.Fortier@kearsarge.org>
2019-03-12 08:03:16 -0400

    missiles lives fixed ( images aren't right)

--- MissileBar.py
+++ MissileBar.py
@@ -1,20 +1,20 @@
 import pygame, sys, math, random
-from Ship import *
+from PlayerShip import *
 from Missile import *
 width = 1100
 height = 900
 
 class MissileBar():
-    def __init__(self, stock, startPos = [0,0]):
+    def __init__(self, missiles, startPos = [0,0]):
         self.images = [pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.1.png"),[200,50]),
+                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.1.png"),[200,50]),
                        pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.2.png"),[200,50]),
-                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.3.png"),[200,50])]         
-                        
+                       pygame.transform.scale(pygame.image.load("Screen Display/HUD/HealthBar/images/RocketBar.3.png"),[200,50])]  
         
-        self.image = self.images[stock]
+        self.image = self.images[missiles]
         self.rect = self.image.get_rect(center=startPos)
 
-    def update(self, health):
-        self.image = self.images[stock]
+    def update(self, missiles):
+        self.image = self.images[missiles]
         
          
