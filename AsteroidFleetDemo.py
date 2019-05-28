9bd4485acf3365deda932b10ecafca50408a4cf2
JJ <john.davis@kearsarge.org>
2019-05-28 07:46:49 -0400

    idk

--- AsteroidFleetDemo.py
+++ AsteroidFleetDemo.py
@@ -115,7 +115,7 @@ while go:
   #-----GAME SETUP-------------
     for bg in backgrounds:
         bg.kill()
-    Background("Screen Display/Background/images/scrolling2.png", True, False, [0, -1800])
+    Background("Screen Display/Background/images/hyper.bg.png", True, False, [0, -1800])
     Background("Screen Display/Background/images/Scrollingspace2.png", True, False, [0, -3600])
     player1 = PlayerShip(2)
     missile = None
