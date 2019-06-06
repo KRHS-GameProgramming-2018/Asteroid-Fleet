dda3ed67537f64625dfba7635892fb8ee6284b01
Jared Fortier <Jared.Fortier@kearsarge.org>
2019-06-06 07:41:23 -0400

    yeet

--- AsteroidFleetDemo.py
+++ AsteroidFleetDemo.py
@@ -248,6 +248,7 @@ while go:
             if special.kind == "nuke":
                 player1.collideNuke()
                 player1.ready = False
+                player1.key = False
             
         for asteroid in asteroidsHitSpecials:
              asteroid.collideNuke(mega)
